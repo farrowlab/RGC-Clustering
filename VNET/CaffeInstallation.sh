659094c5af95aaf1c9af825fa6e4c24f2408ba66
Farrowlab <karl.farrow@nerf.be>
2017-12-15 17:16:06 +0100

    remove VNET

--- VNET/CaffeInstallation.sh
+++
@@ -1,6 +0,0 @@
-pip install protobuf
-echo 'export CAFFE_ROOT=/opt/3D-Caffe' >> ~/.bashrc
-echo 'export PYTHONPATH=/opt/3D-Caffe/distribute/python:$PYTHONPATH' >> ~/.bashrc
-echo 'export PYTHONPATH=/opt/3D-Caffe/python:$PYTHONPATH' >> ~/.bashrc
-echo 'export PATH=/usr/local/cuda-8.0/bin:$PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/opt/3D-Caffe/distribute/lib:/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
--- VNET/PythonLibraryInstallation.sh
+++
@@ -1,4 +0,0 @@
-pip install SimpleITK
-pip install matplotlib
-pip install multiprocessing
-pip install skimage
